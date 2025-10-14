326391e8165fd0876cc0a79f13151e80b9e7de3b
SPECZIN <87613154+SPEC-X67@users.noreply.github.com>
2025-10-14 20:26:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://www.goodberry.store',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: [
             'Content-Type',
